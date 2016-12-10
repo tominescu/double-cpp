bedeffdad4b5e421ad2d163ac234efea23088d05
Xiaofu <tominescu@gmail.com>
2016-12-11 01:15:42 +0800

    update client

--- epoll-server/client.cpp
+++ epoll-server/client.cpp
@@ -9,6 +9,8 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <netdb.h>
+#include <sstream>
 
 using namespace std;
 
@@ -17,6 +19,31 @@ const int DEFAULT_PORT = 9990;
 
 void Usage(const string& name) {
     cerr<<name<<" -p <server_port> server_ip"<<endl;
+}
+
+bool GetIP(const string& remote_addr, string& ip) {
+    sockaddr_in sin;
+    int ret = inet_pton(AF_INET, remote_addr.c_str(), &sin);
+    if (ret > 0) {
+        /* remote addr is an ip address */
+        ip = remote_addr;
+        return true;
+    }
+    hostent *he;
+    in_addr **in_addr_list;
+    he = gethostbyname(remote_addr.c_str());
+    if (he == NULL) {
+        return false;
+    }
+    in_addr_list = (in_addr**)he->h_addr_list;
+    for (int i = 0; in_addr_list[i] != NULL; i++) {
+        ip = inet_ntoa(*in_addr_list[i]);
+        ret = inet_pton(AF_INET, ip.c_str(), &sin);
+        if (ret > 0) {
+            return true;
+        }
+    }
+    return false;
 }
 
 void MainLoop(int sock) {
@@ -92,24 +119,32 @@ int main(int argc, char** argv) {
         handle_error("socket");
     }
 
+    string remote_ip;
+    if (!GetIP(remote_addr, remote_ip)) {
+        cerr<<"can't resolve name of host "<<remote_addr<<endl;
+        return -1;
+    }
+
     sockaddr_in sin;
     bzero(&sin, sizeof(sin));
     sin.sin_family = AF_INET;
-    if (inet_aton(remote_addr.c_str(), &sin.sin_addr) == -1) {
+    if (inet_aton(remote_ip.c_str(), &sin.sin_addr) == -1) {
         handle_error("inet_aton");
     }
     sin.sin_port = htons(remote_port);
 
     if (connect(sock, (sockaddr*)&sin, sizeof(sin)) == -1) {
-        handle_error("connect");
+        stringstream ss;
+        ss << "connect to "<<remote_ip<<":"<<remote_port;
+        handle_error(ss.str().c_str());
     }
-    cerr<<"Connected to "<<remote_addr<<":"<<remote_port<<endl;
+    cerr<<"Connected to "<<remote_ip<<":"<<remote_port<<endl;
     
     SetNonBlock(sock);
     SetSockBufSize(sock, 65536, 65536);
 
     MainLoop(sock);
     close(sock);
-    cerr<<"Disonnected to "<<remote_addr<<":"<<remote_port<<endl;
+    cerr<<"Disonnected to "<<remote_ip<<":"<<remote_port<<endl;
     return 0;
 }
